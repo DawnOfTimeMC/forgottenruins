f1cf1f8c7a15083def07382946dd2adcc4f4c396
pchag <p.chagnon@orange.fr>
2025-10-16 22:00:51 +0200

    Soon done :)

---
+++ neoforge/src/main/java/org/dawnoftime/forgottenruins/ForgottenRuins.java
@@ -0,0 +1,14 @@
+package org.dawnoftime.forgottenruins;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(Constants.MOD_ID)
+public class ForgottenRuins {
+    
+    public ForgottenRuins(IEventBus eventBus) {
+        // This method is invoked by the NeoForge mod loader when it is ready
+        // to load your mod. You can access NeoForge and Common code in this
+        // project.
+    }
+}
